930c2522f5ffba267b93f72e81ab1c08dbd9f3a3
AvanAvi <theastrofreek@gmail.com>
2025-09-26 21:25:26 +0200

    GREEN - /books HTML list page - listBooks method and template - shouldShowBookListPage passing

--- src/main/java/com/attsw/bookstore/web/BookstoreWebController.java
+++ src/main/java/com/attsw/bookstore/web/BookstoreWebController.java
@@ -1,13 +1,25 @@
 package com.attsw.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.attsw.bookstore.repository.BookRepository;
 
 @Controller
 public class BookstoreWebController {
 
+    @Autowired
+    private BookRepository repo;
+
     @GetMapping("/")
     public String home() {
-        return "index";   
+        return "index";
+    }
+
+    @GetMapping("/books")
+    public String listBooks(Model model) {
+        model.addAttribute("books", repo.findAll());
+        return "books/list";
     }
 }
