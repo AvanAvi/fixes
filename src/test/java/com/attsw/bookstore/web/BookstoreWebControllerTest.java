3e819fdb62fe47d0c94bff1855f2e45de0e07b5a
AvanAvi <theastrofreek@gmail.com>
2025-09-26 21:18:18 +0200

    RED - add HTML controller test for /books list page - shouldShowBookListPage failing

---
+++ src/test/java/com/attsw/bookstore/web/BookstoreWebControllerTest.java
@@ -0,0 +1,38 @@
+package com.attsw.bookstore.web;
+
+import static org.mockito.Mockito.when;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.attsw.bookstore.model.Book;
+import com.attsw.bookstore.repository.BookRepository;
+
+@WebMvcTest(BookstoreWebController.class)
+class BookstoreWebControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private BookRepository repo;
+
+    @Test
+    void shouldShowBookListPage() throws Exception {
+        Book b = Book.withTitle("Clean Code");
+        when(repo.findAll()).thenReturn(Arrays.asList(b));
+
+        mvc.perform(get("/books"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("books/list"))
+            .andExpect(model().attributeExists("books"));
+    }
+}
