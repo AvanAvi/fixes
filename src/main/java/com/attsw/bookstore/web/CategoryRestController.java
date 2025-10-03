cc92734107012c6dbc7c671cf3b3f02db763ae08
AvanAvi <theastrofreek@gmail.com>
2025-10-03 23:11:41 +0200

    GREEN: implement POST /api/categories endpoint

--- src/main/java/com/attsw/bookstore/web/CategoryRestController.java
+++ src/main/java/com/attsw/bookstore/web/CategoryRestController.java
@@ -2,6 +2,11 @@ package com.attsw.bookstore.web;
 
 import org.springframework.web.bind.annotation.*;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import java.util.List;
 
 import com.attsw.bookstore.model.Category;
@@ -20,5 +25,11 @@ public class CategoryRestController {
     @GetMapping
     public List<Category> all() {
         return categoryService.getAllCategories();
+    }
+    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Category create(@RequestBody Category category) {
+        return categoryService.saveCategory(category);
     }
 }
